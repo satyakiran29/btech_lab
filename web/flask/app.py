807d9bb0bac981f8bf28fd222161970fd053d7e7
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-05-05 21:06:38 +0530

    Update app.py

--- web/flask/app.py
+++ web/flask/app.py
@@ -12,24 +12,28 @@ def insert_record():
 
 @app.route('/addstudent',methods=['GET','POST'])
 def add_student():
-    if request.method=='POST':
+    if request.method == 'POST':
         try:
-            ueid=request.form['mailid']
-            uname=request.form['username']
-            upwd=request.form['pwd']
+            ueid = request.form['mailid']
+            uname = request.form['username']
+            upwd = request.form['pwd']
             with sqlite3.connect('studentdb.db') as con:
-                cur=con.cursor()
-                cur.execute('INSERT INTO STUDENT(username,email,password) VALUES(?,?,?)',(uname,ueid,upwd))
+                cur = con.cursor()
+                # Create table if it doesn't exist
+                cur.execute('''CREATE TABLE IF NOT EXISTS STUDENT (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                username TEXT NOT NULL,
+                                email TEXT NOT NULL UNIQUE,
+                                password TEXT NOT NULL)''')
+                cur.execute('INSERT INTO STUDENT(username, email, password) VALUES(?, ?, ?)', (uname, ueid, upwd))
                 con.commit()
-                msg='Record successfully Inserted'
+                msg = 'Record successfully Inserted'
         except:
             con.rollback()
-            msg='Insert record Unsuccessfull...TRY AGAIN!'
-            
+            msg = 'Insert record Unsuccessful...TRY AGAIN!'
         finally:
-            return render_template('result_app.html',msg=msg)
+            return render_template('result_app.html', msg=msg)
             con.close()
 
 if __name__=='__main__':
     app.run(debug=True)
-
