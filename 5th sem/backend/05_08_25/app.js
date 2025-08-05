b779b42d1bdca18d14257c20df77ff67a76bcdf8
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-08-05 12:37:41 +0530

    Add basic arithmetic functions and logging
    Introduces a simple Node.js script that logs a welcome message, performs addition and subtraction using both direct operations and functions, and outputs the results to the console.

---
+++ 5th sem/backend/05_08_25/app.js
@@ -0,0 +1,13 @@
+console.log('Welcome to gmrit');
+var a = 20;
+var b = 10;
+console.log('Sum',a+b);
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+console.log('Sum in function',add(2,3));
+
+console.log('Sub in function',sub(2,3));
